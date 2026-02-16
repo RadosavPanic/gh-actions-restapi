62da9d5c638f1f66bd78e86a2d5a29b8541bf1b7
RadosavPanic <radosav.panic@gmail.com>
2026-02-16 09:08:38 +0100

    added testing environment and branches rules

--- app.js
+++ app.js
@@ -13,6 +13,4 @@ app.use(bodyParser.json());
 
 app.use(eventRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT);
