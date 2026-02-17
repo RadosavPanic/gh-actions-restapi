340b311f98fbfac1fd2128af6ce0fd9014947e4c
RadosavPanic <radosav.panic@gmail.com>
2026-02-17 15:35:56 +0100

    changed connection protocol in db and added caching deps condition for actions test

--- data/database.js
+++ data/database.js
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const protocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${protocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log("Pending connection with db..");
